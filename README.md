4c0346e85e1662c17962851f8ea7b82f1ba1728b
Ameer Abdallah <ameeryabdallah1@gmail.com>
2023-12-10 02:46:54 -0800

    Add movie library support;Add support for checking runtime left of entire libraries instead of just individual series

--- README.md
+++ README.md
@@ -29,7 +29,21 @@ python show_runtime_left.py
 ```bash
 python show_runtime_left.py --overwrite-token 
 # or 
-python show_runtime_left.py -o
+python show_runtime_left.py -O
+```
+
+* Calculate for entire library
+```bash
+python show_runtime_left.py --entire-library
+# or
+python show_runtime_left.py -E
+```
+
+* Skip marker calculation. This is useful since markers need to be pulled for each episode of a series 1 by 1. This is parallelized to improve performance but is still orders of magnitude slower than simply skipping the marker calculation.
+```bash
+python show_runtime_left.py --skip-markers
+# or
+python show_runtime_left.py -S
 ```
 
 ## Authentication
--- show_runtime_left.py
+++ show_runtime_left.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from typing import cast, List, Tuple
+from typing import TypeAlias, cast, List, Tuple
 from plexapi.media import Marker
 from plexapi.server import PlexServer
-from plexapi.library import LibrarySection, ShowSection 
-from plexapi.video import Show, Episode
+from plexapi.library import LibrarySection, MovieSection, ShowSection 
+from plexapi.video import Movie, Show, Episode
 from iterfzf import iterfzf
 from tqdm import tqdm
 import datetime
@@ -16,40 +16,74 @@ for includeKey in Episode._INCLUDES:
     Episode._INCLUDES[includeKey] = 0
 Episode._INCLUDES['includeMarkers'] = 1 
 
+RuntimeLeft: TypeAlias = Tuple[int, int]
+
+def clear_screen() -> None:
+    print('\033[H\033[J')
+
 def reload_episode(episode: Episode) -> Episode:
     return episode.reload()
 
-def get_runtime_left(show: Show) -> Tuple[int, int]:
-
+def get_series_runtime_left(show: Show, skip_markers: bool = False, should_print: bool = True, max_marker_workers: int | None = None) -> RuntimeLeft:
     unwatched_episodes = cast(List[Episode], show.unwatched())
-    with ThreadPoolExecutor() as executor:
-        list(tqdm(executor.map(reload_episode, unwatched_episodes), total=len(unwatched_episodes), desc="Fetching episode markers. This may take a while."))
+
+    if not skip_markers:
+        with ThreadPoolExecutor(max_workers=max_marker_workers) as executor:
+            if should_print:
+                 list(tqdm(executor.map(reload_episode, unwatched_episodes), total=len(unwatched_episodes), desc=f'Fetching episode markers for "{show.title}"'))
+            else:
+                 list(executor.map(reload_episode, unwatched_episodes))
 
     total_runtime_with_watched = 0
-    for episode in tqdm(show.episodes(), desc="Calculating total runtime with watched"):
+    for episode in tqdm(show.episodes(), desc=f'Calculating total runtime with watched for "{show.title}"') if should_print else show.episodes():
         total_runtime_with_watched += episode.duration
 
 
     total_runtime_without_watched = 0
 
     total_intro_and_credit_duration = 0
-    if unwatched_episodes.__len__() == 0:
-        print("All episodes are already watched for this series...")
+    if unwatched_episodes.__len__() == 0 and should_print:
+        print(f'All episodes are already watched for "{show.title}".')
         print("Skipping calculation of runtime left...")
     else:
-        for episode in tqdm(unwatched_episodes, desc="Calculating runtime left"): 
+        for episode in tqdm(unwatched_episodes, desc=f'Calculating runtime left for "{show.title}"') if should_print else unwatched_episodes: 
 
-            ## Add to the total intro and credit duration which will later be subtracted from the runtime left
-            markers = [marker for marker in episode.markers if marker.type == 'intro' or marker.type == 'credit']
-            markers = cast(List[Marker], markers)
-            for marker in markers: 
-                marker_duration = marker.end - marker.start
-                total_intro_and_credit_duration += marker_duration
+            if not skip_markers:
+                ## Add to the total intro and credit duration which will later be subtracted from the runtime left
+                markers = [marker for marker in episode.markers if marker.type == 'intro' or marker.type == 'credit']
+                markers = cast(List[Marker], markers)
+                for marker in markers: 
+                    marker_duration = marker.end - marker.start
+                    total_intro_and_credit_duration += marker_duration
 
             total_runtime_without_watched += episode.duration
         total_runtime_without_watched -= total_intro_and_credit_duration
 
-    return (total_runtime_with_watched, int(total_runtime_without_watched))
+    return (int(total_runtime_with_watched), int(total_runtime_without_watched))
+
+def get_movie_runtime_left(movie: Movie, skip_markers: bool = False) -> RuntimeLeft:
+
+    total_runtime_with_watched = movie.duration
+    total_runtime_without_watched = movie.duration
+
+    if not skip_markers:
+        ## Add to the total intro and credit duration which will later be subtracted from the runtime left
+        markers = [marker for marker in movie.markers if marker.type == 'intro' or marker.type == 'credit']
+        markers = cast(List[Marker], markers)
+        total_intro_and_credit_duration = 0
+        for marker in markers: 
+            marker_duration = marker.end - marker.start
+            total_intro_and_credit_duration += marker_duration
+        total_runtime_without_watched -= total_intro_and_credit_duration
+
+    return (int(total_runtime_with_watched), int(total_runtime_without_watched))
+
+def print_runtime_left(runtime: int, runtime_left: int | bool = False) -> None:
+    print('\n')
+    print(f'Total runtime: {datetime.timedelta(milliseconds=runtime)}')
+    if runtime_left:
+        print(f'Runtime left: {datetime.timedelta(milliseconds=runtime_left)}')
+    print('\n')
 
 def isYes(input):
     return input == 'y' or input == 'Y'
@@ -62,37 +96,79 @@ valid_responses = ['y', 'Y', 'n', 'N', '']
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='Calculate the runtime left of a show.')
     args.add_argument('--overwrite-token', '-O', default=False, action='store_true', help='Overwrite the stored token in the keyring.')
+    args.add_argument('--entire-library', '-E', default=False, action='store_true', help='Calculate the runtime left of all shows in the library.')
+    args.add_argument('--skip-markers', '-S', default=False, action='store_true', help='Proceed without the additional calculations for markers.')
     parsed_args = args.parse_args()
 
-    account = handle_plex_authentication(parsed_args.overwrite_token) 
+    overwrite_token = parsed_args.overwrite_token
+    entire_library = parsed_args.entire_library
+    skip_markers = parsed_args.skip_markers
+
+    account = handle_plex_authentication(overwrite_token) 
     
     server_name = None
     if len(account.resources()) == 1:
         server_name = account.resources()[0].name
     else:
-        server_name = iterfzf([server.name for server in account.resources()], prompt='Select server: ')
+        server_names = [server.name for server in account.resources()]
+        server_name = None
+        server_name = iterfzf(server_names, prompt='Select server: ', case_sensitive=False)
 
     plexserver = cast(PlexServer, account.resource(server_name).connect())
 
     while True:
+        clear_screen()
         libraries = cast(List[LibrarySection], plexserver.library.sections())
-        libraries = [library for library in libraries if library.type == 'show']
+        libraries = [library for library in libraries if library.type == 'show' or library.type == 'movie']
+
+        # Select library
         library_titles = [library.title for library in libraries]
-        library_title = iterfzf(library_titles, prompt='Select library: ')
-        library = cast(ShowSection, libraries[library_titles.index(library_title)])
-
-        shows = library.all()
-
-        show_titles = [show.title for show in shows]
-        show_title = iterfzf(show_titles, prompt='Select item: ')
-        show = cast(Show, shows[show_titles.index(show_title)])
+        library_title = None
         
-        runtime_left = get_runtime_left(show)
+        library_title = iterfzf(library_titles, prompt='Select library: ', case_sensitive=False)
+        library = libraries[library_titles.index(library_title)]
 
-        print('\n')
-        print(f'Total runtime: {datetime.timedelta(milliseconds=runtime_left[0])}')
-        print(f'Runtime left: {datetime.timedelta(milliseconds=runtime_left[1])}')
-        print('\n')
+        if library.type == 'show':
+            library = cast(ShowSection, library)
+            shows = library.all()
+
+            total_runtime = 0
+            total_runtime_left = 0
+            if not entire_library:
+                # Select show
+                show_titles = [show.title for show in shows]
+                show_title = None
+                show_title = iterfzf(show_titles, prompt='Select item: ', case_sensitive=False)
+
+                show = cast(Show, shows[show_titles.index(show_title)])
+                runtime_left = get_series_runtime_left(show, skip_markers) 
+                total_runtime += runtime_left[0]
+                total_runtime_left += runtime_left[1]
+            else:
+                # Start incrementing the total runtime and runtime left
+                for show in tqdm(shows, desc="Calculating runtime left for all shows"):
+                    print("\n\n")
+                    # we don't want to get network blocked since we are checking many shows so the max_workers is set to 2
+                    # not necessary when only checking for one show since server not likely to block us for that
+                    runtime_left = get_series_runtime_left(show, skip_markers, max_marker_workers=2)
+                    total_runtime += runtime_left[0]
+                    total_runtime_left += runtime_left[1]
+                    clear_screen()
+            print_runtime_left(total_runtime, total_runtime_left) 
+
+        elif library.type == 'movie':
+            library = cast(MovieSection, library)
+            movies = library.all()
+            total_runtime = 0
+            total_runtime_left = 0
+
+            # Start incrementing the total runtime and runtime left
+            for movie in tqdm(movies, desc="Calculating runtime left for all movies"):
+                runtime_left = get_movie_runtime_left(movie, skip_markers)
+                total_runtime += runtime_left[0]
+                total_runtime_left += runtime_left[1]
+
+            print_runtime_left(total_runtime, total_runtime_left)
 
         user_input = input('Do you want to select another item? [Y/n]: ') 
         while user_input not in valid_responses:
